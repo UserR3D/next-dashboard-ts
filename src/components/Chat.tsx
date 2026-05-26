e2314eda0a1055f7256cd6d93c6594f27d162aeb
Red <kauawar04@gmail.com>
2026-05-26 15:00:05 -0300

    Supabase connection + hook use-realtime

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,106 +1,39 @@
 'use client';
 
-import { supabase } from '@/lib/supabase/client';
+import { useRealTimeChat } from '@/hooks/use-realtime-chat';
 import { useSession } from 'next-auth/react';
-import Image from 'next/image';
 import React from 'react';
-
-type Message = {
-	id: string;
-	content: string;
-	userId: string;
-	user: {
-		name: string;
-		email: string;
-		image: string;
-		customImage: [{ url: string }];
-	};
-	createdAt: string;
-};
 
 export const Chat = () => {
 	const { data: session } = useSession();
-	const [messages, setMessages] = React.useState<Message[]>([]);
-	const [newMessage, setNewMessages] = React.useState('');
-	React.useEffect(() => {
-		const channel = supabase.channel('test-room');
-		channel
-			.on(
-				'postgres_changes',
-				{
-					event: 'INSERT',
-					schema: 'public',
-					table: 'message',
-				},
-				(payload) => {
-					setMessages((prev) => [...prev, payload.new as Message]);
-					console.log(payload, messages);
-				},
-			)
-			.subscribe((status) => console.log(status));
-		return () => {
-			supabase.removeChannel(channel);
-		};
-	}, [messages]);
-
-	React.useEffect(() => {
-		async function loadMessages() {
-			const { data, error } = await supabase
-				.from('message')
-				.select(`*, user:User (name, email, image, customImage:Image (url))`);
-			if (error) console.error(error.message);
-			if (data) setMessages(data);
-			console.log(data);
-		}
-		loadMessages();
-	}, []);
-
-	async function sendMessages(e: React.SubmitEvent) {
-		e.preventDefault();
-		if (!session?.user || !newMessage.trim()) return null;
-		const { data, error } = await supabase
-			.from('message')
-			.insert({
-				content: newMessage,
-				userId: session.user.id,
-			})
-			.select();
-		if (error) console.error(error.message);
-		if (data && data[0]) setMessages((prev) => [...prev, data[0]]);
-		setNewMessages('');
-	}
-	if (!messages) return null;
+	const [content, setContent] = React.useState('');
+	const { messages, sendMessages, isConnected } = useRealTimeChat('teste-room');
+	if (!isConnected) return <p>Connection failed</p>;
 	return (
 		<div>
-			{messages.map((item, id) => {
+			{messages.length === 0 ? <div>No messages found yet</div> : null}
+			{messages.map((message, index) => {
 				return (
-					<div key={id}>
-						<p>{item.content}</p>
-						<p>{item.user.name}</p>
-						<p>{item.user.email}</p>
-						{item.user.image || item.user.customImage[0].url ? (
-							<Image
-								alt="User Profile Chat"
-								src={item.user.image || item.user.customImage[0].url}
-								width={50}
-								height={50}
-							/>
-						) : null}
-						<p>{item.createdAt}</p>
-					</div>
+					<ul key={index}>
+						<li>{message.content}</li>
+						<li>{message.id}</li>
+					</ul>
 				);
 			})}
-			<form id="myForm" onSubmit={sendMessages}>
-				<input
-					onChange={(e) => setNewMessages(e.target.value)}
-					className="border"
-					type="text"
-					placeholder={!session ? 'Please log in' : 'Write a message'}
-				/>
-				<button type="submit" form="myForm">
-					Send
-				</button>
-			</form>
+			<label>Chat</label>
+			<input
+				onChange={(e) => setContent(e.target.value)}
+				className="border"
+				type="text"
+			/>
+			<button
+				onClick={async (e) => {
+					e.preventDefault();
+					await sendMessages(content, session?.user.name ?? 'Anonymous');
+				}}
+			>
+				Butao
+			</button>
 		</div>
 	);
 };
---
+++ src/hooks/use-realtime-chat.tsx
@@ -0,0 +1,69 @@
+import { createClient } from '@/lib/supabase/client';
+import React from 'react';
+
+export interface ChatMessage {
+	id: string;
+	content: string;
+	user: {
+		name: string;
+	};
+	createdAt: string;
+}
+
+const EVENT_MESSAGE_TYPE = 'message';
+const supabase = createClient();
+export const useRealTimeChat = (roomName: string) => {
+	const [channel, setChannel] = React.useState<ReturnType<
+		typeof supabase.channel
+	> | null>(null);
+	const [isConnected, setIsConnected] = React.useState(false);
+	const [messages, setMessages] = React.useState<ChatMessage[]>([]);
+	React.useEffect(() => {
+		const newChannel = supabase.channel(roomName, {
+			config: {
+				private: true,
+			},
+		});
+
+		newChannel
+			.on('broadcast', { event: EVENT_MESSAGE_TYPE }, (payload) => {
+				setMessages((current) => [...current, payload.payload as ChatMessage]);
+			})
+			.subscribe((status) => {
+				if (status === 'SUBSCRIBED') {
+					setIsConnected(true);
+				} else {
+					setIsConnected(false);
+				}
+			});
+		setChannel(newChannel);
+		return () => {
+			supabase.removeChannel(newChannel);
+		};
+	}, [roomName]);
+
+	const sendMessages = React.useCallback(
+		async (content: string, username: string) => {
+			console.log(isConnected);
+			if (!channel || !isConnected)
+				return console.error('Channel broadcast error');
+			const message = {
+				id: crypto.randomUUID(),
+				content,
+				user: {
+					name: username,
+				},
+				createdAt: new Date().toUTCString(),
+			};
+			setMessages((current) => [...current, message]);
+			return await channel?.send({
+				type: 'broadcast',
+				event: EVENT_MESSAGE_TYPE,
+				payload: message,
+			});
+		},
+		[channel, isConnected],
+	);
+
+	return { messages, sendMessages, isConnected };
+};
