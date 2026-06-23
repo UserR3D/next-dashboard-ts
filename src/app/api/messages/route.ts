a78b9f873f2a6b929a8290994896df3f32801bc1
Red <kauawar04@gmail.com>
2026-06-23 02:10:17 -0300

    message route json reformuled

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -2,12 +2,14 @@ import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 import { NextRequest } from 'next/server';
 import { serviceMessage } from '../services/serviceMessage';
+import { handleServer } from '@/lib/serverHandling';
 
 export async function POST(request: NextRequest) {
 	const session = await getServerSession(authOptions);
 	const { content } = await request.json();
 	if (!session?.user?.id) {
-		return null;
+		return Response.json(handleServer({ error: `Don't authenticated` }, 401));
 	}
-	return Response.json(serviceMessage(session.user.id, content));
+	const messageCreated = serviceMessage(session.user.id, content);
+	return Response.json(messageCreated);
 }
